a4aa0aa8517b18ee192d1d0cfa96f869083363e9
yoshiki0425 <judging5@gmail.com>
2022-01-22 13:24:00 +0900

    テストコミット

---
+++ app/src/main/java/com/example/mystartapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mystartapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telecom.TelecomManager
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //テスト
+    }
+}
